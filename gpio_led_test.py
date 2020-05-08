47ccf4668d057453ae2976f54bcaf4579ae645e7
faberc <kpfaber@gmail.com>
2020-05-08 15:31:57 -0700

    1

--- gpio_led_test.py
+++ gpio_led_test.py
@@ -11,14 +11,14 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(23, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 print ("LED On")
 
-GPIO.output(23, GPIO.HIGH)
+GPIO.output(18, GPIO.HIGH)
 
 time.sleep(1)
 
 print ("LED off")
 
-GPIO.output(23, GPIO.LOW)
+GPIO.output(18, GPIO.LOW)
